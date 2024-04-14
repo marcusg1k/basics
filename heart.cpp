89e250fbb7f06591d398f94476b4eec9054cd2ac
Marcus <102842400+marcusg1k@users.noreply.github.com>
2024-04-14 18:38:48 -0500

    Add files via upload

---
+++ heart.cpp
@@ -0,0 +1,27 @@
+// marcusg1k
+#include <iostream> 
+
+
+int main() {
+    int a, b, size = 20; // letters in the heart and the size of the heart
+// you can change the letters in the int and the size for a bigger heart print
+    for(a = size/2; a <= size; a = a+2) 
+    {
+        for (b = 1; b < size-a; b = b+2)
+            std::cout << " "; // 
+        for (b = 1; b <= a; b++)
+            std::cout << "a"; 
+        for (b = 1; b <= size-a; b++)
+            std::cout << " "; 
+        for (b = 1; b <= a-1; b++)
+            std::cout << "a"; 
+        std::cout << std::endl;  
+    }
+        for (a = size; a>= 0; a--){
+            for (b = a; b < size; b++)
+                std::cout << " "; 
+            for(b = 1; b <= ((a*2) - 1); b++)
+            std::cout << "b";
+          std::cout << std::endl; 
+        }
+    }
