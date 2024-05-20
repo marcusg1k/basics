a2babca8fe4dd93b43d3a611030075d7bdda9907
Marcus <marcusgutierrezufc@gmail.com>
2024-05-19 20:57:51 -0500

    temperature converter

--- tempconverter.cpp
+++ tempconverter.cpp
@@ -10,9 +10,8 @@ int main()
     // so to convert f to c we use the formula c = (f - 32) * 5/9
     // and to convert c to f we use the formula f = (c * 9/5) + 32
     std::cout << "Enter a temp in fahrenheit: "; 
-    std::cin >> f; 
-    c = (f - 32) * 5/9; 
-    std::cout << f << " degrees fahrenheit is " << c << " degrees celsius." << std::endl; 
+    std::cin >> f;  
+    std::cout << f << " degrees fahrenheit is " << (c = f - 32) * 5/9 << " degrees celsius." << std::endl; 
     std::cout << "Enter a temp in celsius: "; 
     std::cin >> c; 
     std::cout << c << " degrees celsius is " << (c * 9/5) + 32 << " degrees fahrenheit." << std::endl; 
